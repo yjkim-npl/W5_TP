b01cd6e65b648f9a816c9c2de86fb3b229ea078b
hongzina <ghdjindk1350@gmail.com>
2024-05-17 20:19:06 +0900

    [추가]캐릭터 무브

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerInputController : TotalController
+{
+    private Camera camera;
+
+    
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        // Vector 값을 실수로 변환
+        {
+            CallLookEvent(newAim);
+        }
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Debug.Log("OnFire" + value.ToString());
+    }
+}
---
+++ Assets/Scripts/TotalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class TotalController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+}
