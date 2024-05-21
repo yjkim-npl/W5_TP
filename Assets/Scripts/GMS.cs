5023c0c6884d10261ace603340183fe982a21b81
hongzina <ghdjindk1350@gmail.com>
2024-05-21 21:07:04 +0900

    [추가] 멀티 엔드판낼 완성
    .

--- Assets/Scripts/GMS.cs
+++ Assets/Scripts/GMS.cs
@@ -10,6 +10,10 @@ public class GMS : MonoBehaviour
     [SerializeField] private TMP_Text scoreTxt;
     [SerializeField] private GameObject Player1;
     [SerializeField] private GameObject Player2;
+    [SerializeField] private GameObject endPannel;
+    [SerializeField] private GameObject multiEndPannel;
+    [SerializeField] private TMP_Text winnerPannel;
+    [SerializeField] private TMP_Text loserPannel;
     public static GMS instance;
 
     private float time;
@@ -78,8 +82,29 @@ public class GMS : MonoBehaviour
         scoreTxt.text = "진행 시간" + "\n" + time.ToString("N2");
     }
 
-    public void GameOver()
+    public void GameOver(GameObject loser)
     {
         Time.timeScale = 0f;
+        if (DM.instance.GetMode() == 1)
+        {
+            endPannel.gameObject.SetActive(true);
+
+        }
+        if (DM.instance.GetMode() == 2)
+        {
+            multiEndPannel.gameObject.SetActive(true);
+            //이름 받아오기
+            if(loser == Player1)
+            {
+                loserPannel.text = loser.GetComponentInChildren<TMP_Text>().text;
+                winnerPannel.text = Player2.GetComponentInChildren<TMP_Text>().text;
+            }
+            else if(loser == Player2)
+            {
+                loserPannel.text = loser.GetComponentInChildren<TMP_Text>().text;
+                winnerPannel.text = Player1.GetComponentInChildren<TMP_Text>().text;
+            }
+            //승 패여부에 따라 판넬에 넣기
+        }
     }
 }
